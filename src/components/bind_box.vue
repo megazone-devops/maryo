09f5d5c6011edaee8bedb18c6312fa808f0e290a
Eojin <pio500@naver.com>
2019-09-23 15:48:47 +0900

    cut, copy, paste, delete 기능 완료

--- src/components/bind_box.vue
+++ src/components/bind_box.vue
@@ -1,21 +1,29 @@
 <template>
 	<div class="box_style" :style="color">
-		
+		{{address}}
 		<div class="box_header" v-if="title">
+			<div class="menu_bar">
+				<b-button v-if="title!='' || typeof(data)=='string'" variant="outline-primary" class="header_button" @click="flag=!flag">{{flag==true? '-':'+'}}</b-button>
+				<div class="right_button_box">
+					<b-button variant="outline-primary" class="right_button" @click="cut_item">CUT</b-button>
+					<b-button variant="outline-primary" class="right_button" @click="copy_item">COPY</b-button>
+					<b-button variant="outline-primary" class="right_button" @click="paste_item">PASTE</b-button>
+					<b-button variant="outline-primary" class="right_button" @click="delete_item">DEL</b-button>
+				</div>
+			</div>
 			<h2 >{{title}}</h2>
-			<b-button v-if="title!='' || typeof(data)=='string'" variant="outline-primary" class="header_button" @click="flag=!flag">{{flag==true? '-':'+'}}</b-button>
 		</div>
 		<div v-if="flag==true">
 			<div v-if="typeof(data[0])=='string'&&typeof(data[1])=='string'">
 				{{data[0]}} : {{data[1]}}
 			</div>
 			<div v-else-if="isNaN(data[0])==true && typeof(data[1])=='object'">
-				<bind_box v-for="(item, index) in Object.entries(data[1])" :key="index" :title="isNaN(data[0])==true? data[0]:''" :data=item :color_arr="[color_arr[0],color_arr[1]+1]"></bind_box>
+				<bind_box v-for="(item, index) in Object.entries(data[1])" :key="index" :title="''" :data=item :color_arr="[color_arr[0],color_arr[1]+1]" :address="address"></bind_box>
 			</div>
 			<div v-else>
 				<div v-for="(item,index) in Object.entries(data[1])" :key="index">
 					<div v-if="typeof(item[1])=='object'">
-						<bind_box :data=[0,item[1]] :color_arr="[color_arr[0],color_arr[1]+1]" :title="isNaN(item[0])==true? item[0]:''"></bind_box>
+						<bind_box :data=[index,item[1]] :color_arr="[color_arr[0],color_arr[1]+1]" :title="isNaN(item[0])==true? item[0]:''" :address="isNaN(item[0])==true? address.concat([data[0],item[0]]):address"></bind_box>
 					</div>
 					<div v-else-if="isNaN(item[0])==false && typeof(item[1])=='string'">
 						{{item[1]}}
@@ -23,11 +31,9 @@
 					<div v-else>
 						{{item[0]}} : {{item[1]}}
 					</div>
-						
 				</div>
 			</div>
 		</div>
-			
 	</div>
 </template>
 
@@ -36,7 +42,7 @@ import color_set from '../assets/color_set/color_set.json'
 import bind_box from './bind_box'
 export default {
   name: 'bind_box',
-  props: ['data','title','color_arr'],
+  props: ['data','title','color_arr','address'],
   data() {
     return {
 	  flag:true,
@@ -48,9 +54,29 @@ export default {
   },
   created(){
 	this.color = "background:"+this.color_set[this.color_arr[0]][this.color_arr[1]]
-	  console.log(this.color_set[this.color_arr[0]][this.color_arr[1]])
+
   },
   methods: {
+	copy_item() {
+		this.$bvToast.toast(`Copied`, {
+			title: 'Alert',
+			autoHideDelay: 3000
+		})
+		this.$store.commit('copyData', this.address);
+	},
+	cut_item() {
+		this.$bvToast.toast(`Cuted`, {
+			title: 'Alert',
+			autoHideDelay: 3000
+		})
+		this.$store.commit('cutData', this.address);
+	},
+	delete_item(){
+		this.$store.commit('deleteData', this.address);
+	},
+	paste_item(){
+		this.$store.commit('pasteData', this.address);
+	}
   }
 }
 </script>
@@ -62,14 +88,25 @@ export default {
 }
 .box_header{
 	width:100%;
-	height:50px;
 	position:relative
 }
 .header_button{
 	top:5px;
-	right:5px;
+	left:5px;
 	position:absolute;
 	width:40px;
 	height:40px;
+}
+.right_button_box{
+	float:right;
+	padding-top:5px;
+	padding-right:5px;
+}
+.right_button{
+	float:left;
+}
+.menu_bar{
+	display: inline-block;
+	width: 100%;
 }
 </style>
